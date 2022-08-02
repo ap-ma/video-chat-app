0b51dca2fa3ebf7bd19fc167c9ea96654c8bd879
kensuke-nagata <steeelcore@gmail.com>
2022-08-03 00:08:47 +0900

    デプロイシェル修正

--- backend/sh/deploy.sh
+++ backend/sh/deploy.sh
@@ -3,7 +3,7 @@
 mkdir -p /opt/app
 
 pushd `dirname $(cd $(dirname $0); pwd)`
-sudo -l $(logname) -c "cd $(pwd) && cargo build --release"
+su -l $(logname) -c "cd $(pwd) && cargo build --release"
 cp ./chat-app-api /opt/app/
 cp ./chat-app-api.service /etc/systemd/system/
 popd
